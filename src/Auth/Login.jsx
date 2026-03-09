9961b7935cff45eb1d670826fc9776e71760bcf4
Kutty <godwinraj845@gmail.com>
2026-03-09 21:17:42 +0530

    Updated

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://profile-management-backend-2jxo.onrender.com/api/auth/login",
         { email, password },
         { headers: { "Content-Type": "application/json" } }
       );
